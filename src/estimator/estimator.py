222bd0c21d0e2cc5d0642d0d85a4125823324182
serhii dolhopolov <serhiidolhopolovcoder@gmail.com>
2025-04-12 07:35:52 +0300

    changes

--- src/estimator/estimator.py
+++ src/estimator/estimator.py
@@ -2,8 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class Estimator(ABC):   
-    def __init__(self):
-        pass
+    def __init__(self, train_X, train_y, test_X, test_y):
+        self.train_X = train_X
+        self.train_y = train_y
+        self.test_X = test_X
+        self.test_y = test_y
         
     @abstractmethod
     def fit(self):
--- src/estimator/forest_estimator.py
+++ src/estimator/forest_estimator.py
@@ -4,16 +4,16 @@ from estimator import Estimator
 
 
 class ForestEstimator(Estimator):
-    def __init__(self, params=None):
-        super().__init__()
+    def __init__(self, train_X, train_y, test_X, test_y, params=None):
+        super().__init__(train_X, train_y, test_X, test_y)
         if params is None:
             params = dict()
         self.model = RandomForestClassifier(**params)
 
-    def fit(self, X, y):
-        self.model.fit(X, y)
-        print(self.model.score(X, y))
+    def fit(self):
+        self.model.fit(self.train_X, self.train_y)
+        print(self.model.score(self.train_X, self.train_y))
 
-    def predict(self, X):
-        y_train_predict = self.model.predict(X)
+    def predict(self):
+        y_train_predict = self.model.predict(self.train_X)
         print(y_train_predict)
